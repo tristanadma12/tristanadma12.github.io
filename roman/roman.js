2a5c6b1956072a26294d7e3d18f5a52f2f76934c
tristanadma12 <tristanadma12@gmail.com>
2024-05-28 22:34:42 +0700

    Add files via upload

---
+++ roman/roman.js
@@ -0,0 +1,6 @@
+const btnPropose = document.getElementById("btn-propose");
+const responseDiv = document.getElementById("response");
+
+btnPropose.addEventListener("click", function() {
+	responseDiv.innerHTML = "Yeayyy! Makasih ayaangg. Love You In every universe <3";
+});
